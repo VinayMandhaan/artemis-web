11b5263ff5fb61745455ff1ee503198df0bae8c1
VinayMandhaan <vinaykumar@ITs-MacBook-Pro.local>
2025-01-16 17:08:15 +0400

    Create Item API Integrated

--- app/page.js
+++ app/page.js
@@ -2,19 +2,45 @@
 import styles from "./styles.module.scss";
 import Block from "./components/Block";
 import { useEffect, useState } from "react";
+import { baseUrl } from "./utils/baseUrl";
 export default function Home() {
   const [data, setNewData] = useState([])
   const [selectedItems, setSelectedItems] = useState([])
+  const [loading, setLoading] = useState(false)
 
-  
+
   const getData = async () => {
     try {
-      const res = await fetch("http://localhost:5000/api/blocks")
+      const res = await fetch(`${baseUrl}/blocks`)
       const data = await res.json()
       setNewData(data?.blocks)
     } catch (error) {
       console.log(error)
     }
+  }
+
+  const handleSubmit = async () => {
+    setLoading(true)
+    let blockId = selectedItems.map((item) => item._id)
+
+    fetch(`${baseUrl}/items`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ blockId })
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        setLoading(false)
+        setSelectedItems([])
+        alert('Submitted Successfully')
+      })
+      .catch(error => {
+        console.error('Error:', error)
+        setLoading(false)
+      })
   }
 
   useEffect(() => {
@@ -34,7 +60,9 @@ export default function Home() {
       </div>
       {
         data?.length > 0 && (
-          <button disabled={selectedItems?.length === 0} className={selectedItems?.length === 0 ? styles.btnDisabled : styles.btnMain}>Submit</button>
+          <button onClick={() => {
+            handleSubmit()
+          }} disabled={selectedItems?.length === 0 || loading} className={(selectedItems?.length === 0 || loading) ? styles.btnDisabled : styles.btnMain}>Submit</button>
         )
       }
     </main>
